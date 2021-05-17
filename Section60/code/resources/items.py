49dbe3c7a4d613d4a05c75bd7d600894e6f255f1
Tyrone <zekah54@gmail.com>
2021-05-17 02:44:01 +0000

    Models and Resources Breakdown

---
+++ Section60/code/resources/items.py
@@ -0,0 +1,72 @@
+from flask_restful import Resource,reqparse
+from flask_jwt import jwt_required
+import sqlite3
+from models.item import ItemModel
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type = float,required = True, help = "This field cannot be blank")
+
+    @jwt_required()
+    def get(self,name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
+
+    def post(self,name):
+        # Checking if an item with such a name already exists...
+        if ItemModel.find_by_name(name):
+            return {"message": "An item with the name '{}' already exists.".format(name)}, 400
+        data = Item.parser.parse_args()
+        item = ItemModel(name,data['price'])
+
+        try:
+            item.insert()
+        except:
+            return {'message': 'An error occured in insertion'}, 500 #Internal Server Error
+
+        return item.json(), 201
+
+    def delete(self,name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name = ?;"
+        cursor.execute(query,(name,))
+        connection.commit()
+        connection.close()
+        return {'message': 'Item Deleted'}
+
+    def put(self,name):   
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name,data['price'])
+
+        if(item is None):
+            try:
+                updated_item.insert()
+            except:
+                return {'message': 'An error occured in input'}, 500
+        else:
+            try:
+                updated_item.update()
+            except:
+                return {'message': 'An error occured in update'}, 500
+        return updated_item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items"
+        results = cursor.execute(query)
+        items = []
+        for row in results:
+            items.append({'name':row[0],'price': row[1]})
+        connection.commit()
+        connection.close()
+
+        return {'item': items}
